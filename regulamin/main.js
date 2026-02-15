f9cb08a334520f5affe616b7f6515f0d674517d0
nieskoit <64593623+nieskoit@users.noreply.github.com>
2026-02-15 15:12:16 +0100

    Add files via upload

---
+++ regulamin/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('%c Kraina Twórców %c System załadowany pomyślnie ', 
+                'background: #76c043; color: white; padding: 2px 5px; border-radius: 3px 0 0 3px;', 
+                'background: #242424; color: #76c043; padding: 2px 5px; border-radius: 0 3px 3px 0;');
+});
